d91d3742c350968455aedeb25c936efec4d6d7ba
Abdulrahaman27 <abdulrahamanabbas27@gmail.com>
2025-10-19 08:59:42 +0100

    Cleaned up routes

--- routes/web.php
+++ routes/web.php
@@ -10,31 +10,7 @@ use App\Http\Controllers\Users\UsersController;
 // });
 Auth::routes();
 Route::get('/', [PropertiesController::class, 'index'])->name('home');
-Route::get('/props/prop-details/{id}', [PropertiesController::class, 'single'])->name('single.prop');
-
-
-// Inserting requests
-Route::post('/props/prop-details/{id}', [PropertiesController::class, 'insertRequest'])->name('insert.request');
-
-// Inserting saved properties
-Route::post('/props/prop-details/save/{id}', [PropertiesController::class, 'saveProps'])->name('save.prop');
-// Deleting saved properties
-Route::delete('/props/prop-details/delete/{id}', [PropertiesController::class, 'deleteProps'])->name('delete.prop');
-
-// get properties by type (Buy) 
-Route::get('/props/type/Buy', [PropertiesController::class, 'propsBuy'])->name('buy.prop');
-
-// get properties by type (Rent) 
-Route::get('/props/type/Rent', [PropertiesController::class, 'propsRent'])->name('rent.prop');
-
-// displaying property by home_type
-Route::get('/props/home-type/{hometype}', [PropertiesController::class, 'displayByHomeType'])->name('display.prop.hometype');
-
-// users pages
-Route::get('/user/all-requests', [UsersController::class, 'allRequests'])->name('all.requests');
-
-// displaying saved properties
-Route::get('/user/saved-properties', [UsersController::class, 'savedProperties'])->name('saved.props');
+Route::get('/home', [PropertiesController::class, 'index'])->name('home');
 
 // display contact page
 Route::get('contact', [HomeController::class, 'contact'])->name('contact');
@@ -42,8 +18,40 @@ Route::get('contact', [HomeController::class, 'contact'])->name('contact');
 // display about page
 Route::get('about', [HomeController::class, 'about'])->name('about');
 
-// displaying property by PRICE ASCENDING
-Route::get('/props/price-asc', [PropertiesController::class, 'priceAsc'])->name('price.asc.prop');
+Route::group(['prefix' => 'props'], function () {
+    Route::get('prop-details/{id}', [PropertiesController::class, 'single'])->name('single.prop');
 
-// displaying property by PRICE DESCENDING
-Route::get('/props/price-desc', [PropertiesController::class, 'priceDesc'])->name('price.desc.prop');
+    // Inserting requests
+    Route::post('prop-details/{id}', [PropertiesController::class, 'insertRequest'])->name('insert.request');
+    
+    // Inserting saved properties
+    Route::post('prop-details/save/{id}', [PropertiesController::class, 'saveProps'])->name('save.prop');
+    // Deleting saved properties
+    Route::delete('prop-details/delete/{id}', [PropertiesController::class, 'deleteProps'])->name('delete.prop');
+    
+    // get properties by type (Buy) 
+    Route::get('type/Buy', [PropertiesController::class, 'propsBuy'])->name('buy.prop');
+    
+    // get properties by type (Rent) 
+    Route::get('type/Rent', [PropertiesController::class, 'propsRent'])->name('rent.prop');
+    
+    // displaying property by home_type
+    Route::get('home-type/{hometype}', [PropertiesController::class, 'displayByHomeType'])->name('display.prop.hometype');
+
+    // displaying property by PRICE ASCENDING
+    Route::get('price-asc', [PropertiesController::class, 'priceAsc'])->name('price.asc.prop');
+
+    // displaying property by PRICE DESCENDING
+    Route::get('price-desc', [PropertiesController::class, 'priceDesc'])->name('price.desc.prop');
+
+});
+
+
+Route::group(['prefix' => 'user'], function () {
+    // users pages
+    Route::get('all-requests', [UsersController::class, 'allRequests'])->name('all.requests');
+    
+    // displaying saved properties
+    Route::get('saved-properties', [UsersController::class, 'savedProperties'])->name('saved.props');
+});
+
